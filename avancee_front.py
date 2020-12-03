0ff4983b0e202579b74f3aad41b7925838b3e6b1
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-03 11:39:16 +0100

    Modèle python pour déterminer le temps de résidence

---
+++ avancee_front.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pylab
+
+
+C_0 = 1
+U = 2e-8
+D = 10**(-9)
+tannee = 317
+t = tannee * 365 * 24 * 3600
+# t = 10_000_000_000
+
+def f(x):
+    return (C_0/2)*(math.erfc((x - U*t)/(2*math.sqrt(D*t))) + np.exp(-(U*x)/D)*math.erfc((x + U*t)/(2*math.sqrt(D*t))))
+
+x = np.linspace(0, 420, 420)
+y = np.array([f(i) for i in x])
+plt.plot(x,y)
+plt.show()
